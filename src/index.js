fae2439b51a0c0510a7c28fe7ae5e51250ebf038
Kostiantyn <41370966+Kostiantyn-S@users.noreply.github.com>
2021-01-26 22:20:28 +0200

    start html + styles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-import './styles/index.scss';
-import './assets/fonts/Roboto-Regular.ttf';
-import './component.js';
-
-const app = document.getElementById('app');
-
-const greeting = 'Hello World';
-
-app.innerHTML = `
-    <strong class="main-title">
-        ${greeting}. Webpack работает!
-    </strong>
-`;
+import './styles/styles.scss';
